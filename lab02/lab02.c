9fc96cb6b1b512e1f57c0364844b29fe1fdadfec
Cloud User <ec2-user@ip-172-31-38-30.ec2.internal>
2024-03-05 18:53:32 +0000

    Made changes on lab02.c

--- lab02/lab02.c
+++ lab02/lab02.c
@@ -1,26 +1,71 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
 
-int main()
-{
- pid_t pid; /* fork a child process */
-int  
-pid = fork();
- if (pid < 0) { /* error occurred */
- fprintf(stderr, "Fork Failed\n");
- return 1;
- }
- else if (pid == 0) { /* child process */
- printf("I am the child %d\n",pid);
- execlp("/bin/ls","ls",NULL);
- }
- else { /* parent process */
- /* parent will wait for the child to complete */
- printf("I am the parent %d\n",pid);
- wait(NULL);
+#define BUFFER_SIZE 256
 
- printf("Child Complete\n");
- }
- return 0;
+int main() {
+    int pipefd[2];
+    pid_t pid;
+    char message_to_child[BUFFER_SIZE];
+    char message_to_parent[BUFFER_SIZE];
+    char concatenated_message[BUFFER_SIZE * 2];
+
+ 
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        exit(EXIT_FAILURE);
+    }
+
+ 
+    pid = fork();
+
+    if (pid == -1) {
+        perror("fork");
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) {
+        close(pipefd[1]);
+
+        
+        read(pipefd[0], message_to_child, BUFFER_SIZE);
+        printf("Child process received message: %s\n", message_to_child);
+
+       
+        char reply[] = "Message received by child process!";
+        write(pipefd[0], reply, strlen(reply) + 1);
+
+        close(pipefd[0]); 
+    } else {
+        close(pipefd[0]); 
+
+     
+        printf("Enter message to send to child process: ");
+        fgets(message_to_parent, BUFFER_SIZE, stdin);
+        message_to_parent[strcspn(message_to_parent, "\n")] = '\0'; 
+        write(pipefd[1], message_to_parent, strlen(message_to_parent) + 1);
+
+    
+        read(pipefd[1], message_to_parent, BUFFER_SIZE);
+        printf("Parent process received reply: %s\n", message_to_parent);
+
+    
+        printf("Enter additional string to concatenate: ");
+        fgets(concatenated_message, BUFFER_SIZE, stdin);
+        concatenated_message[strcspn(concatenated_message, "\n")] = '\0'; 
+
+        strcat(message_to_parent, concatenated_message);
+
+        printf("Concatenated message: %s\n", message_to_parent);
+
+        close(pipefd[1]);
+
+       
+        wait(NULL);
+    }
+
+    return 0;
 }
